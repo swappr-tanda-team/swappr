48151e2f2e75adb271ccdd21af2e5ba4a5e4e05a
BlueDragon23 <aidan_goldthorpe@outlook.com>
2016-04-23 12:43:27 +1000

    Added calendar import, changed shift display to table

--- swappr/shift/views.py
+++ swappr/shift/views.py
@@ -10,6 +10,7 @@ from swappr.models import User
 from swappr.user.tanda_api import tanda_auth
 from . import tanda_shift
 import pprint
+from calendar import day_name
 
 shift = Blueprint('shift', __name__, url_prefix='/shift')
 
@@ -21,4 +22,4 @@ def user_shifts():
     List the shifts that belong to the user
     """
     upcoming_shifts = tanda_shift.fetch_current_user_upcoming_shifts()
-    return render_template('shift/your_shifts.html', upcoming_shifts=upcoming_shifts)
+    return render_template('shift/your_shifts.html', upcoming_shifts=upcoming_shifts, days=day_name)
