5880bb76ef14253e539d3f4587b620d269073cb3
Dave Finster <davefinster@me.com>
2016-04-23 10:24:51 +1000

    extract shift fetch logic into separate file

--- swappr/shift/views.py
+++ swappr/shift/views.py
@@ -8,6 +8,7 @@ from swappr import login_manager, app
 from swappr.database import db_session
 from swappr.models import User
 from swappr.user.tanda_api import tanda_auth
+from . import tanda_shift
 import pprint
 
 shift = Blueprint('shift', __name__, url_prefix='/shift')
@@ -19,14 +20,5 @@ def user_shifts():
     """
     List the shifts that belong to the user
     """
-    shift_info = tanda_auth.get('rosters/current', token=(session["tanda_oauth"]["access_token"],)).data
-    valid_shifts = []
-    for i in range(len(shift_info["schedules"])):
-    	#at this point, we examining all the schedules for a particular day
-    	for j in range(len(shift_info["schedules"][i]["schedules"])):
-    		sched_item = shift_info["schedules"][i]["schedules"][j]
-    		if (sched_item["user_id"] == current_user.employee_id):
-    			sched_item["date"] = shift_info["schedules"][i]["date"]
-    			valid_shifts.append(sched_item)
-    print(valid_shifts)
-    return render_template('user/account.html')
+    upcoming_shifts = tanda_shift.fetch_current_user_upcoming_shifts()
+    return render_template('your_shifts.html')
